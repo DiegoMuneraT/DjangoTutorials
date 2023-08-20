2fda7419623689b041536c4c2ad26d2f429f26a9
Diego <damunerat@eafit.edu.co>
2023-08-20 18:01:50 -0500

    Tutorial #1 Terminado

--- tutoriales_project/pages/views.py
+++ tutoriales_project/pages/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.views import View
+from django import forms
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -22,13 +23,13 @@ class AboutPageView(TemplateView):
     
 class Product:
     products = [
-        {"id":"1", "name":"TV", "description":"Best TV"},
-        {"id":"2", "name":"iPhone", "description":"Best iPhone"},
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"},
-        {"id":"4", "name":"Glasses", "description":"Best Glasses"}
+        {"id":"1", "name":"TV", "description":"Best TV", "price": 110},
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price": 50},
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price": 45},
+        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price": 23},
     ]
 
-class ProductIndexView(View):
+class ProductIndexView(View):        
     template_name = 'products/index.html'
 
     def get(self, request):
@@ -45,9 +46,38 @@ class ProductShowView(View):
 
     def get(self, request, id):
         viewData = {}
+        
+        if int(id) > len(Product.products):
+            return render(request, 'pages/home.html')
+        
         product = Product.products[int(id)-1]
         viewData["title"] = product["name"] + " - Online Store"
         viewData["subtitle"] =  product["name"] + " - Product information"
         viewData["product"] = product
 
         return render(request, self.template_name, viewData)
+    
+class ProductForm(forms.Form):
+    name = forms.CharField(required=True)
+    price = forms.FloatField(required=True)
+
+class ProductCreateView(View):
+    template_name = 'products/create.html'
+
+    def get(self, request):
+        form = ProductForm()
+        viewData = {}
+        viewData["title"] = "Create product"
+        viewData["form"] =  form
+        return render(request, self.template_name, viewData)
+    
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid:
+            return redirect(form)
+        else:
+            viewData = {}
+            viewData["title"] = "Create product"
+            viewData["form"] =  form
+            return render(request, self.template_name, viewData)
+
